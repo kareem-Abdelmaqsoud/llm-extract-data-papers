43750ac8b53e74f7ed5efb93e983f7fe522b4307
Kareem Abdelmaqsoud <kabdelma@andrew.cmu.edu>
2025-01-25 18:01:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -142,8 +142,10 @@ if uploaded_file:
     col1, col2, col3 = st.columns([1, 2, 1])
     with col1:
         if st.button("Previous Table", disabled=(current_index == 0)):
-            st.session_state.current_table_index -= 1
+            st.session_state.current_table_index = max(0,current_index -1)
+            st.experimental_rerun()
 
     with col3:
         if st.button("Next Table", disabled=(current_index == len(st.session_state.tables) - 1)):
-            st.session_state.current_table_index += 1
+            st.session_state.current_table_index = min(len(st.session_state.tables) - 1), current_index +1)
+            st.experimental_rerun()
